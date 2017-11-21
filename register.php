dc3e20519f6156a8991672ba9101052757d1e131
Rohit Singh Rajawat <rajawat.rohit8@gmail.com>
2017-11-21 11:51:30 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,259 @@
+<?php
+ $server="localhost";
+$username="id3688264_rohit";
+$password="rohit888";
+$db="id3688264_rohitdb";
+$conn=mysqli_connect($server,$username,$password,$db);
+$error="";
+/*$server="localhost";
+$username="learn_php";
+$password="hello123";
+$db="skill_spread";
+$conn=mysqli_connect($server,$username,$password,$db);
+*/
+if(isset($_POST['submit']))
+{
+    $uname=mysqli_real_escape_string($conn,$_POST['uname']);
+    $password=mysqli_real_escape_string($conn,$_POST['upassword']);
+    $target="";
+    $sql="select * from logintable where email='$uname' AND password='$password'" ;
+    $run=mysqli_query($conn,$sql);
+    $rows=mysqli_num_rows($run);
+    if($rows==1)
+    {
+        header("location: index.php");
+    }
+    else
+    {
+        $error="Incorrect ID or Password";
+    }
+   
+   
+    
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title> E-commerce Website</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+		<!-- bootstrap -->
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">      
+		<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">		
+		<link href="themes/css/bootstrappage.css" rel="stylesheet"/>
+		
+		<!-- global styles -->
+		<link href="themes/css/flexslider.css" rel="stylesheet"/>
+		<link href="themes/css/main.css" rel="stylesheet"/>
+
+		<!-- scripts -->
+		<script src="themes/js/jquery-1.7.2.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>				
+		<script src="themes/js/superfish.js"></script>	
+		<script src="themes/js/jquery.scrolltotop.js"></script>
+		<!--[if lt IE 9]>			
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+    <body>		
+		<div id="top-bar" class="container">
+			<div class="row">
+				<div class="span4">
+					<form method="POST" class="search_form">
+						<input type="text" class="input-block-level search-query" Placeholder="eg. T-sirt">
+					</form>
+				</div>
+				<div class="span8">
+					<div class="account pull-right">
+						<ul class="user-menu">				
+							<li><a href="#">My Account</a></li>
+							<li><a href="cart.php">Your Cart</a></li>
+							<li><a href="checkout.php">Checkout</a></li>					
+							<li><a href="register.php">Login</a></li>		
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="wrapper" class="container">
+			<section class="navbar main-menu">
+				<div class="navbar-inner main-menu">				
+					<a href="index.html" class="logo pull-left"><img src="themes/images//logo.png" class="site_logo" alt=""></a>
+					<nav id="menu" class="pull-right">
+						<ul>
+							<li><a href="./products.html">Woman</a>					
+								<ul>
+									<li><a href="./products.php">Lacinia nibh</a></li>									
+									<li><a href="./products.php">Eget molestie</a></li>
+									<li><a href="./products.php">Varius purus</a></li>									
+								</ul>
+							</li>															
+							<li><a href="./products.php">Man</a></li>			
+							<li><a href="./products.php">Sport</a>
+								<ul>									
+									<li><a href="./products.php">Gifts and Tech</a></li>
+									<li><a href="./products.php">Ties and Hats</a></li>
+									<li><a href="./products.php">Cold Weather</a></li>
+								</ul>
+							</li>							
+							<li><a href="./products.php">Hangbag</a></li>
+							<li><a href="./products.php">Best Seller</a></li>
+							<li><a href="./products.php">Top Seller</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>			
+			<section class="header_text sub">
+			<img class="pageBanner" src="themes/images/pageBanner.png" alt="New products" >
+				<h4><span>Login or Register</span></h4>
+			</section>			
+			<section class="main-content">				
+				<div class="row">
+					<div class="span5">					
+						<h4 class="title"><span class="text"><strong>Login</strong> Form</span></h4>
+						<form action="#" method="post">
+							<input type="hidden" name="next" value="/">
+							<fieldset>
+								<div class="control-group">
+									<label class="control-label">Username</label>
+									<div class="controls">
+										<input type="text" placeholder="Enter your username" id="username" class="input-xlarge" name="uname">
+									</div>
+								</div>
+								<div class="control-group">
+									<label class="control-label">Password</label>
+									<div class="controls">
+										<input type="password" placeholder="Enter your password" id="password" class="input-xlarge"  name="upassword" >
+									</div>
+								</div>
+								<div class="control-group">
+									<input tabindex="3" class="btn btn-inverse large" type="submit" value="Sign into your account" name="submit">
+									<hr>
+									<p class="reset">Recover your <a tabindex="4" href="#" title="Recover your username or password">username or password</a></p>
+								</div>
+							</fieldset>
+						</form>				
+					</div>
+					<div class="span7">					
+						<h4 class="title"><span class="text"><strong>Register</strong> Form</span></h4>
+						<form action="#" method="post" class="form-stacked">
+							<fieldset>
+								<div class="control-group">
+									<label class="control-label">Username</label>
+									<div class="controls">
+										<input type="text" placeholder="Enter your username" class="input-xlarge" name="reguser">
+									</div>
+								</div>
+								<div class="control-group">
+									<label class="control-label">Email address:</label>
+									<div class="controls">
+										<input type="email" placeholder="Enter your email" class="input-xlarge" name="regemail">
+									</div>
+								</div>
+								<div class="control-group">
+									<label class="control-label">Password:</label>
+									<div class="controls">
+										<input type="password" placeholder="Enter your password" class="input-xlarge" name="regpassword">
+									</div>
+								</div>							                            
+								<div class="control-group">
+									<p>Now that we know who you are. I'm not a mistake! In a comic, you know how you can tell who the arch-villain's going to be?</p>
+								</div>
+								<hr>
+								<div class="actions"><input tabindex="9" class="btn btn-inverse large" type="submit" value="Create your account" name="submit_request"></div>
+							</fieldset>
+						</form>					
+					</div>				
+				</div>
+			</section>			
+			<section id="footer-bar">
+				<div class="row">
+					<div class="span3">
+						<h4>Navigation</h4>
+						<ul class="nav">
+							<li><a href="./index.php">Homepage</a></li>  
+							<li><a href="./about.php">About Us</a></li>
+							<li><a href="./contact.php">Contac Us</a></li>
+							<li><a href="./cart.php">Your Cart</a></li>
+							<li><a href="./register.php">Login</a></li>							
+						</ul>					
+					</div>
+					<div class="span4">
+						<h4>My Account</h4>
+						<ul class="nav">
+							<li><a href="#">My Account</a></li>
+							<li><a href="#">Order History</a></li>
+							<li><a href="#">Wish List</a></li>
+							<li><a href="#">Newsletter</a></li>
+						</ul>
+					</div>
+					<div class="span5">
+						<p class="logo"><img src="themes/images/logo.png" class="site_logo" alt=""></p>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. the  Lorem Ipsum has been the industry's standard dummy text ever since the you.</p>
+						<br/>
+						<span class="social_icons">
+							<a class="facebook" href="#">Facebook</a>
+							<a class="twitter" href="#">Twitter</a>
+							<a class="skype" href="#">Skype</a>
+							<a class="vimeo" href="#">Vimeo</a>
+						</span>
+					</div>					
+				</div>	
+			</section>
+			<section id="copyright">
+				<span>Copyright 2013 bootstrappage template  All right reserved.</span>
+			</section>
+		</div>
+		<script src="themes/js/common.js"></script>
+		<script>
+			$(document).ready(function() {
+				$('#checkout').click(function (e) {
+					document.location.href = "checkout.php";
+				})
+			});
+		</script>		
+    </body>
+</html>
+<?php
+if(isset($_POST['submit_request'])){
+	echo $name=mysqli_real_escape_string($conn,strip_tags($_POST['reguser']));
+	echo $email=mysqli_real_escape_string($conn,strip_tags($_POST['regemail']));
+	echo $password=mysqli_real_escape_string($conn,strip_tags($_POST['regpassword']));
+	
+	$ins_sql="INSERT INTO logintable (username,email,password) VALUES('$name','$email','$password')";
+	if(mysqli_query($conn,$ins_sql)){
+		?>
+		
+		
+		<?php
+	}
+}
+?>
